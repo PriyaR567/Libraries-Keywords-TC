972ffe1e628d029671ffe50417fe4b91b136e74e
PriyadarshiniRajendran <105422230+PriyaR567@users.noreply.github.com>
2022-05-25 07:46:18 +0530

    Update Activites.py

--- Libraries&Keywords/Activites.py
+++ Libraries&Keywords/Activites.py
@@ -1,35 +1,18 @@
 # Write code To find the value of pi using math library 
 
-# import math
-# print(math.pi)
 
 
 
 
 # Write code To find the sum of three decimal numbers using decimal library
 
-from decimal import Decimal
 
-a = Decimal(input('Enter the first decimal number:- '))
-b = Decimal(input('Enter the second decimal number:- '))
-c = Decimal(input('Enter the third decimal number:- '))
-
-print (a+b+c)
 
 
 
 
 # Write code To generate a random number between 0 to 9 using the random library 
 
-# import random
-# print(random.randint(0,9))
 
 
 # Write code To perform concatenation between strings. 
-
-# Taking input from the user
-# name = input("Enter your name: ")
- 
-# # Output
-# print("Hello, " + name)
-# print(type(name))
